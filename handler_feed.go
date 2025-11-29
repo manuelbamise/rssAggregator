d274fefc7bcf2770194b1ee38f9ec5d0761430db
manuelbamise <etobi1930@gmail.com>
2025-11-29 16:01:22 +0100

    process: finished with the function converting the structure of the return feed from the database to the user

---
+++ handler_feed.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/manuelbamise/rssAggregator/internal/database"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithERROR(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		return
+	}
+	newFeed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+		Url:       params.URL,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		respondWithERROR(w, 400, fmt.Sprintf("Error creating new feed: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(newUser))
+}
